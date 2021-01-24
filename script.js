48bb62db23a24379e872e354902a5636ab2d2a23
Greg Seiler <ashimar229@gmail.com>
2021-01-23 20:21:02 -0500

    Added forecast and did more styling

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const weatherLbl = document.getElementById('weather');
 const pressureLbl = document.getElementById('pressure')
 const dirArrow = document.getElementById('dirArrow');
 const windSpeedLbl = document.getElementById('wind-speed');
+const forecastUL = document.getElementById('forecast-ul');
+
 
 let currTempUnit = 'F';
 let currTemp;
@@ -20,12 +22,15 @@ let weather;
 let pressure;
 let direction;
 let windSpeed;
+let forecast = [];
+
 
 function changeTempUnit(type){
     document.getElementById(currTempUnit).classList.remove('btn-selected');
     currTempUnit = type;
     document.getElementById(currTempUnit).classList.add('btn-selected');
     showTemp(); 
+    showForecast();
 }
 
 function convertTemp(temp){
@@ -57,9 +62,19 @@ function showTemp(){
     sunUpLbl.innerHTML = "Sunrise: " + dateUP.toLocaleTimeString();
     sunDownLbl.innerHTML = "Sundown: " + dateDown.toLocaleTimeString();
     pressureLbl.innerHTML = "Pressure: " + ((pressure/3386)*100).toFixed(2) + " inHg";
-    windSpeedLbl.innerHTML =  `${compassDirection()} at ${windSpeed}mph`;
+    windSpeedLbl.innerHTML =  `  ${compassDirection()} at ${windSpeed}mph`;
     dirArrow.style.transform = 'rotate(' + (direction-45) + "deg)";
     
+}
+
+function showForecast(){
+    let liTemplate = ["<li class='fc-style bdr-radius color-tile'>" , "</li>"];
+    let daysWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    forecastUL.innerHTML = "";
+    for(let i = 0; i < forecast.length; i++){
+        let date = new Date(forecast[i].dt*1000);
+        forecastUL.innerHTML = forecastUL.innerHTML + liTemplate[0] + daysWeek[date.getDay()] + " " + date.toLocaleTimeString()  + " Temp: " + convertTemp(forecast[i].main.temp).toFixed(2) + '\u00B0' + " - " + forecast[i].weather[0].description + liTemplate[1];
+    }
 }
 
 function logTempData(tempData){
@@ -75,6 +90,18 @@ function logTempData(tempData){
     direction = tempData.wind.deg;
     windSpeed = tempData.wind.speed;
     showTemp();
+}
+
+
+
+function logForecastData(forecastData){
+    console.log(forecastData);
+    forecast = forecastData.list.reduce((tot, item) => {
+        tot.push(item);
+        return tot;
+    },[]);
+    console.log(forecast);
+    showForecast();
 }
 
 let latitude= 0;
@@ -97,6 +124,12 @@ function showPosition(position){
     fetch(weatherUrl)
         .then(response => response.json())
         .then(data => logTempData(data));
+
+    let forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon="+ longitude +"&appid=";
+
+    fetch(forecastUrl)
+        .then(response => response.json())
+        .then(data => logForecastData(data));
 }
 
 getLocation();
